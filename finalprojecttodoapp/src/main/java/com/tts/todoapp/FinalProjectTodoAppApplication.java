6cb98915f38ea182158564c7a7abd10dc4c5c4f7
dvld <nikoroberts620@gmail.com>
2019-07-02 14:58:12 -0500

    init commit

--- finalprojecttodoapp/src/main/java/com/tts/todoapp/FinalProjectTodoAppApplication.java
+++ finalprojecttodoapp/src/main/java/com/tts/todoapp/FinalProjectTodoAppApplication.java
@@ -1,13 +1,13 @@
-package com.tts.finalproject;
+package com.tts.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApp {
+public class FinalProjectTodoAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoApp.class, args);
+		SpringApplication.run(FinalProjectTodoAppApplication.class, args);
 	}
 
 }
